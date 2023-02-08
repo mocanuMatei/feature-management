6462f740e5d40483d1a1e6f21cce618db1b79c5f
mateiMocanu <malasoft.dev@gmail.com>
2023-02-08 14:39:57 +0100

    Added initial classes

---
+++ src/main/java/com/swisscom/featuresmanagement/FeaturesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.swisscom.featuresmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturesManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeaturesManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swisscom/featuresmanagement/FeaturesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swisscom.featuresmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeaturesManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
